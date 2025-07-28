36f0ccfc9dcc8cf7ea998d61b396c1bce2ca67a9
Dashrath Menaria <dashrathmenaria007@gmail.com>
2025-07-28 16:14:15 +0530

    average calc

---
+++ average-calculator/src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Link from "next/link";
+import styles from "./page.module.css";
+import Clock from "./clock/Clock";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Average Calculator",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <div className={styles.container}>
+          <aside className={styles.sidebar}> 
+             
+            <Clock /> 
+                   
+          </aside>
+          <main className={styles.main}>{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
